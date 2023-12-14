c3742bbb2f492785cadae79bdd2296c0d0a83302
GolasalaPuneeth <97512630+GolasalaPuneeth@users.noreply.github.com>
2023-12-14 09:36:37 +0530

    Update Toddler.ino
    Enhance user experience by implementing quiz mode logic for last row and incorporating updates.

--- Toddler.ino
+++ Toddler.ino
@@ -10,14 +10,16 @@
 #include "DFRobotDFPlayerMini.h"
 const byte ROWS = 7; // Seven Horizontal rows
 const byte COLS = 6; // Six Vertical columns
+bool Alphabet_state = false ;
+bool Number_state = false ;
 char keys[ROWS][COLS] = {
-    {'<', '=', '>', '?', '@', 'A'},
-    {'B', ' ', 'C', 'D', ' ', 'E'},
-    {'F', ' ', 'G', 'H', ' ', 'I'},
-    {'J', ' ', 'K', 'L', ' ', 'M'},
-    {'N', ' ', 'O', 'P', ' ', 'Q'},
-    {'R', ' ', 'S', 'T', ' ', 'U'},
-    {'V', 'W', 'X', 'Y', 'Z', '['}};
+    {'=', '>', '?', '@', 'A', 'B'},
+    {'C', ' ', 'D', 'E', ' ', 'F'},
+    {'G', ' ', 'H', 'I', ' ', 'J'},
+    {'K', ' ', 'L', 'M', ' ', 'N'},
+    {'O', ' ', 'P', 'Q', ' ', 'R'},
+    {'S', ' ', 'T', 'U', ' ', 'V'},
+    {'W', '[', 'X', 'Y', ']', 'Z'}};
 byte led1 = PB7;
 byte led2 = PB6;
 int count = 0;
@@ -31,7 +33,7 @@ void AphalbetQuiz() // Quiz mode
 {
     digitalWrite(led1, HIGH);
     digitalWrite(led2, LOW);
-    myDFPlayer.play(33);
+    myDFPlayer.play(36);
     delay(3000);
     Serial.println("Quiz Mode Acivated..");
     while (1)
@@ -41,7 +43,7 @@ void AphalbetQuiz() // Quiz mode
             flag = 1;
             break;
         }
-        int ran = random(60, 86);
+        int ran = random(61, 91);
         Serial.println(ran);
         myDFPlayer.play(ran);
         delay(2000);
@@ -51,7 +53,7 @@ void AphalbetQuiz() // Quiz mode
             int num = key;
             if (num == ran && key != NO_KEY)
             {
-                myDFPlayer.play(87);
+                myDFPlayer.play(34);
                 Serial.println("TRUE");
                 Serial.println(ran);
                 Serial.println(num);
@@ -65,18 +67,18 @@ void AphalbetQuiz() // Quiz mode
             { // completed
                 count = 0;
                 Serial.println("WRONG ANSWER..");
-                myDFPlayer.play(86);
-                delay(4000);
+                myDFPlayer.play(33);
+                delay(4900);
                 break;
             }
-            if (num == 90 && key != NO_KEY)
+            if (num == 93 && key != NO_KEY)
             { // completed
                 count = 0;
                 flag = 0;
                 Serial.println("inner loop breaked.");
                 break;
             }
-            if (num == 87 && key != NO_KEY)
+            if (num == 91 && key != NO_KEY)
             { // completed
                 count = 0;
                 flag = 0;
@@ -87,15 +89,15 @@ void AphalbetQuiz() // Quiz mode
             { // incomlete
                 Serial.println(key);
                 count++;
-                myDFPlayer.play(88);
-                delay(1500);
+                myDFPlayer.play(35);
+                delay(1900);
             }
             if (count == 2 && key != NO_KEY)
             { // incomplete
                 Serial.println("WARNING..");
                 count++;
                 myDFPlayer.play(ran);
-                delay(3500);
+                delay(1500);
             }
         }
     }
@@ -104,7 +106,7 @@ void NumberQuiz() // Quiz mode
 {
     digitalWrite(led1, HIGH);
     digitalWrite(led2, LOW);
-    myDFPlayer.play(33);
+    myDFPlayer.play(36);
     delay(3000);
     Serial.println("Quiz Mode Acivated..");
     while (1)
@@ -114,7 +116,7 @@ void NumberQuiz() // Quiz mode
             flag = 1;
             break;
         }
-        int ran = random(60, 86);
+        int ran = random(61, 91);
         Serial.println(ran);
         myDFPlayer.play(ran * 2);
         delay(2000);
@@ -124,7 +126,7 @@ void NumberQuiz() // Quiz mode
             int num = key;
             if (num == ran && key != NO_KEY)
             {
-                myDFPlayer.play(87);
+                myDFPlayer.play(34);
                 Serial.println("TRUE");
                 Serial.println(ran);
                 Serial.println(num);
@@ -138,18 +140,18 @@ void NumberQuiz() // Quiz mode
             { // completed
                 count = 0;
                 Serial.println("WRONG ANSWER..");
-                myDFPlayer.play(86);
-                delay(4000);
+                myDFPlayer.play(33);
+                delay(4900);
                 break;
             }
-            if (num == 90 && key != NO_KEY)
+            if (num == 93 && key != NO_KEY)
             { // completed
                 count = 0;
                 flag = 0;
                 Serial.println("inner loop breaked.");
                 break;
             }
-            if (num == 87 && key != NO_KEY)
+            if (num == 91 && key != NO_KEY)
             { // completed
                 count = 0;
                 flag = 0;
@@ -160,21 +162,23 @@ void NumberQuiz() // Quiz mode
             { // incomlete
                 Serial.println(key);
                 count++;
-                myDFPlayer.play(88);
-                delay(1500);
+                myDFPlayer.play(35);
+                delay(1900);
             }
             if (count == 2 && key != NO_KEY)
             { // incomplete
                 Serial.println("WARNING..");
                 count++;
                 myDFPlayer.play(ran * 2);
-                delay(3500);
+                delay(1500);
             }
         }
     }
 }
 void Alphabetmode()
 {
+    Number_state = true;
+    Alphabet_state = false;
     digitalWrite(led1, LOW);
     digitalWrite(led2, HIGH);
     Serial.println("ALPHABET MODE");
@@ -182,12 +186,12 @@ void Alphabetmode()
     while (1)
     {
         char key = keypad.getKey();
-        if (key == 'W' && key != NO_KEY)
+        if (key == '[' && key != NO_KEY)
         {
             AphalbetQuiz();
             break;
         }
-        if (key == 'Z' && key != NO_KEY)
+        if (key == ']' && key != NO_KEY)
         {
             break;
         }
@@ -195,153 +199,153 @@ void Alphabetmode()
         {
             switch (key)
             {
-            case '<':
+            case '=':
                 Serial.println("A");
                 myDFPlayer.play(2);
                 delay(1500);
                 break;
-            case '=':
+            case '>':
                 Serial.println("B");
                 myDFPlayer.play(3);
                 delay(1500);
                 break;
-            case '>':
+            case '?':
                 Serial.println("C");
                 myDFPlayer.play(4);
                 delay(1500);
                 break;
-            case '?':
+            case '@':
                 Serial.println("D");
                 myDFPlayer.play(5);
                 delay(1500);
                 break;
-            case '@':
+            case 'A':
                 Serial.println("E");
                 myDFPlayer.play(6);
                 delay(1500);
                 break;
-            case 'A':
+            case 'B':
                 Serial.println("F");
                 myDFPlayer.play(7);
                 delay(1500);
                 break;
-            case 'B':
+            case 'C':
                 Serial.println("G");
                 myDFPlayer.play(8);
                 delay(1500);
                 break;
-            case 'C':
+            case 'D':
                 Serial.println("H");
                 myDFPlayer.play(9);
                 delay(1500);
                 break;
-            case 'D':
+            case 'E':
                 Serial.println("I");
                 myDFPlayer.play(10);
                 delay(1500);
                 break;
-            case 'E':
+            case 'F':
                 Serial.println("J");
                 myDFPlayer.play(11);
                 delay(1500);
                 break;
-            case 'F':
+            case 'G':
                 Serial.println("K");
                 myDFPlayer.play(12);
                 delay(1500);
                 break;
-            case 'G':
+            case 'H':
                 Serial.println("L");
                 myDFPlayer.play(13);
                 delay(1500);
                 break;
-            case 'H':
+            case 'I':
                 Serial.println("M");
                 myDFPlayer.play(14);
                 delay(1500);
                 break;
-            case 'I':
+            case 'J':
                 Serial.println("N");
                 myDFPlayer.play(15);
                 delay(1500);
                 break;
-            case 'J':
+            case 'K':
                 Serial.println("O");
                 myDFPlayer.play(16);
                 delay(1500);
                 break;
-            case 'K':
+            case 'L':
                 Serial.println("P");
                 myDFPlayer.play(17);
                 delay(1500);
                 break;
-            case 'L':
+            case 'M':
                 Serial.println("Q");
                 myDFPlayer.play(18);
                 delay(1500);
                 break;
-            case 'M':
+            case 'N':
                 Serial.println("R");
                 myDFPlayer.play(19);
                 delay(1500);
                 break;
-            case 'N':
+            case 'O':
                 Serial.println("S");
                 myDFPlayer.play(20);
                 delay(1500);
                 break;
-            case 'O':
+            case 'P':
                 Serial.println("T");
                 myDFPlayer.play(21);
                 delay(1500);
                 break;
-            case 'P':
+            case 'Q':
                 Serial.println("U");
                 myDFPlayer.play(22);
                 delay(1500);
                 break;
-            case 'Q':
+            case 'R':
                 Serial.println("V");
                 myDFPlayer.play(23);
                 delay(1500);
                 break;
-            case 'R':
+            case 'S':
                 Serial.println("W");
                 myDFPlayer.play(24);
                 delay(1500);
                 break;
-            case 'S':
+            case 'T':
                 Serial.println("X");
                 myDFPlayer.play(25);
                 delay(1500);
                 break;
-            case 'T':
+            case 'U':
                 Serial.println("Y");
                 myDFPlayer.play(26);
                 delay(1500);
                 break;
-            case 'U':
+            case 'V':
                 Serial.println("Z");
                 myDFPlayer.play(27);
                 delay(1500);
                 break;
-            case 'V':
-                Serial.println("CRAZY SMILE");
+            case 'W':
+                Serial.println("COMMON SMILE");
                 myDFPlayer.play(28);
                 delay(1500);
                 break;
-            case '[':
-                Serial.println("SILENT EMOJI");
+            case 'X':
+                Serial.println("THUMBSUP EMOJI");
                 myDFPlayer.play(29);
                 delay(1500);
                 break;
-            case 'X':
-                Serial.println("THUMBS UP");
+            case 'Y':
+                Serial.println("CRAZY SMILE");
                 myDFPlayer.play(30);
                 delay(1500);
                 break;
-            case 'Y':
-                Serial.println("COMMON SMILE");
+            case 'Z':
+                Serial.println("SILENT EMOJI");
                 myDFPlayer.play(31);
                 delay(1500);
                 break;
@@ -351,20 +355,22 @@ void Alphabetmode()
 }
 void Numbersmode()
 {
+    Number_state = false;
+    Alphabet_state = true;
     digitalWrite(led1, LOW);
     digitalWrite(led2, HIGH);
     Serial.println("NUMBER MODE");
-    myDFPlayer.play(89);
+    myDFPlayer.play(91);
     delay(2000);
     while (1)
     {
         char key = keypad.getKey();
-        if (key == 'Z' && key != NO_KEY)
+        if (key == ']' && key != NO_KEY)
         {
             NumberQuiz();
             break;
         }
-        if (key == 'W' && key != NO_KEY)
+        if (key == '[' && key != NO_KEY)
         {
             break;
         }
@@ -372,154 +378,154 @@ void Numbersmode()
         {
             switch (key)
             {
-            case 'A':
+            case 'B':
                 Serial.println("RED");
-                myDFPlayer.play(90);
-                delay(1500);
-                break;
-            case '@':
-                Serial.println("GREEN");
-                myDFPlayer.play(91);
-                delay(1500);
-                break;
-            case '?':
-                Serial.println("PURPLE");
                 myDFPlayer.play(92);
                 delay(1500);
                 break;
-            case '>':
-                Serial.println("BLUE");
+            case 'A':
+                Serial.println("GREEN");
                 myDFPlayer.play(93);
                 delay(1500);
                 break;
-            case '=':
-                Serial.println("YELLOW");
+            case '@':
+                Serial.println("PURPLE");
                 myDFPlayer.play(94);
                 delay(1500);
                 break;
-            case '<':
-                Serial.println("PINK");
+            case '?':
+                Serial.println("BLUE");
                 myDFPlayer.play(95);
                 delay(1500);
                 break;
-            case 'E':
-                Serial.println("WHITE");
+            case '>':
+                Serial.println("YELLOW");
                 myDFPlayer.play(96);
                 delay(1500);
                 break;
-            case 'D':
-                Serial.println("ONE");
+            case '=':
+                Serial.println("PINK");
                 myDFPlayer.play(97);
                 delay(1500);
                 break;
-            case 'C':
-                Serial.println("TWO");
+            case 'F':
+                Serial.println("WHITE");
                 myDFPlayer.play(98);
                 delay(1500);
                 break;
-            case 'B':
-                Serial.println("BLACK");
+            case 'E':
+                Serial.println("ONE");
                 myDFPlayer.play(99);
                 delay(1500);
                 break;
-            case 'I':
-                Serial.println("THREE");
+            case 'D':
+                Serial.println("TWO");
                 myDFPlayer.play(100);
                 delay(1500);
                 break;
-            case 'H':
-                Serial.println("FOUR");
+            case 'C':
+                Serial.println("BLACK");
                 myDFPlayer.play(101);
                 delay(1500);
                 break;
-            case 'G':
-                Serial.println("FIVE");
+            case 'J':
+                Serial.println("THREE");
                 myDFPlayer.play(102);
                 delay(1500);
                 break;
-            case 'F':
-                Serial.println("SIX");
+            case 'I':
+                Serial.println("FOUR");
                 myDFPlayer.play(103);
                 delay(1500);
                 break;
-            case 'M':
-                Serial.println("SEVEN");
+            case 'H':
+                Serial.println("FIVE");
                 myDFPlayer.play(104);
                 delay(1500);
                 break;
-            case 'L':
-                Serial.println("EIGHT");
+            case 'G':
+                Serial.println("SIX");
                 myDFPlayer.play(105);
                 delay(1500);
                 break;
-            case 'K':
-                Serial.println("NINE");
+            case 'N':
+                Serial.println("SEVEN");
                 myDFPlayer.play(106);
                 delay(1500);
                 break;
-            case 'J':
-                Serial.println("ZERO");
+            case 'M':
+                Serial.println("EIGHT");
                 myDFPlayer.play(107);
                 delay(1500);
                 break;
-            case 'Q':
-                Serial.println("SQUARE");
+            case 'L':
+                Serial.println("NINE");
                 myDFPlayer.play(108);
                 delay(1500);
                 break;
-            case 'P':
-                Serial.println("HEART");
+            case 'K':
+                Serial.println("ZERO");
                 myDFPlayer.play(109);
                 delay(1500);
                 break;
-            case 'O':
-                Serial.println("DIMOND");
+            case 'R':
+                Serial.println("SQUARE");
                 myDFPlayer.play(110);
                 delay(1500);
                 break;
-            case 'N':
-                Serial.println("RECTANGLE");
+            case 'Q':
+                Serial.println("HEART");
                 myDFPlayer.play(111);
                 delay(1500);
                 break;
-            case 'U':
-                Serial.println("TRIANGLE");
+            case 'P':
+                Serial.println("DIMOND");
                 myDFPlayer.play(112);
                 delay(1500);
                 break;
-            case 'T':
-                Serial.println("MINUS");
+            case 'O':
+                Serial.println("RECTANGLE");
                 myDFPlayer.play(113);
                 delay(1500);
                 break;
-            case 'S':
-                Serial.println("MULTIPLCATION");
+            case 'V':
+                Serial.println("TRIANGLE");
                 myDFPlayer.play(114);
                 delay(1500);
                 break;
-            case 'R':
-                Serial.println("STAR");
+            case 'U':
+                Serial.println("MINUS");
                 myDFPlayer.play(115);
                 delay(1500);
                 break;
-            case '[':
-                Serial.println("CIRCLE");
+            case 'T':
+                Serial.println("MULTIPLCATION");
                 myDFPlayer.play(116);
+                delay(1500);
+                break;
+            case 'S':
+                Serial.println("STAR");
+                myDFPlayer.play(117);
+                delay(1500);
+                break;
+            case 'Z':
+                Serial.println("CIRCLE");
+                myDFPlayer.play(118);
                 delay(1500);
                 break;
             case 'X':
                 Serial.println("EQUAL");
-                myDFPlayer.play(117);
+                myDFPlayer.play(119);
                 delay(1500);
                 break;
             case 'Y':
                 Serial.println("PLUS");
-                myDFPlayer.play(118);
+                myDFPlayer.play(120);
                 delay(1500);
                 break;
-            case 'V':
+            case 'W':
                 Serial.println("OVEL");
-                myDFPlayer.play(119);
+                myDFPlayer.play(121);
                 delay(1500);
                 break;
             }
@@ -548,6 +554,7 @@ void setup()
     }
     myDFPlayer.volume(30);
     myDFPlayer.play(1);
+    delay(1500);
 }
 void loop()
 {
@@ -558,10 +565,18 @@ void loop()
     {
         switch (key)
         {
-        case 'W':
+        case '[':
+        if(Alphabet_state){
+                  myDFPlayer.play(60);
+                  delay(3000);
+            }
             Alphabetmode();
             break;
-        case 'Z':
+        case ']':
+            if(Number_state){
+                  myDFPlayer.play(60);
+                  delay(3000);
+            }
             Numbersmode();
             break;
         }
